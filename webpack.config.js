2cc9f3ce9314a9d5decbca18ba00f70b039beef7
Shinkei <shinkeygentatsu@yahoo.com.mx>
2017-03-20 21:24:14 -0500

    webpack project with a plugin to generate the html and add the bundle and a css file that is compiled fron a scss file

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: 'dist',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {test: /\.scss$/,
+             use: ExtractTextPlugin.extract({
+                 fallbackLoader: 'style-loader',
+                 use: ['css-loader', 'sass-loader'],
+                 publicPath: '/dist'
+                 })
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'titulo perro',
+           // minify: {
+           //     collapseWhitespace: true  
+           // },
+            hash: true,
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin({
+            filename: 'app.css',
+            disable: false,
+            allChunks: true
+        })
+    ]
+}
